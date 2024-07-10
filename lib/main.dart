7d01faffa47fc2f713cf87fe617413e211ba86a1
pavlysamir <bavlysamir33@gmail.com>
2024-07-11 02:56:55 +0300

    Add HomeScreen widget and initialize service locator and shared preferences cash helper in main()
    This commit adds a new HomeScreen widget to the project's home screen presentation views. Additionally, it initializes the service locator and shared preferences cash helper in the main() function using async/await. The SimpleBlocObserver is also set as the Bloc observer.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:route_task/core/utils/service_locator.dart';
+import 'package:route_task/core/utils/shared_preferences_cash_helper.dart';
+import 'package:route_task/core/utils/simple_bloc_observer.dart';
 import 'package:route_task/route_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setUpServiceLocator();
+  await getIt.get<CashHelperSharedPreferences>().init();
+  Bloc.observer = SimpleBlocObserver();
+
   runApp(const RouteTaskApp());
 }
